c0ab94479176956381cc3d866623d0fea4c385af
DepriFromEarth <107975725+DepriFromEarth@users.noreply.github.com>
2022-06-22 13:12:53 -0400

    Add files via upload

--- Razer Synapse Debloat.bat
+++ Razer Synapse Debloat.bat
@@ -1,10 +1,13 @@
 @echo off
+taskkill /f /im "Razer Synapse 3.exe"
+taskkill /f /im "Razer Synapse Service.exe"
+taskkill /f /im "Razer Synapse Service Process.exe"
+taskkill /f /im "GameManagerService.exe"
+taskkill /f /im "Razer Updater.exe"
+taskkill /f /im "RzUpdateEngineService.exe"
+taskkill /f /im "Razer Central.exe"
 net stop RzActionSvc /y
 sc config RzActionSvc start=demand
-taskkill /f /im GameManagerService.exe
-taskkill /f /im Razer Synapse Service.exe
-taskkill /f /im RzUpdateEngineService.exe
-taskkill /f /im RzUpdateEngineService.exe
 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Razer Synapse Service" /v "Start" /t REG_DWORD /d "3" /f > nul 2>&3
 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Razer Update Service" /v "Start" /t REG_DWORD /d "3" /f > nul 2>&3
 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Razer Game Manager Service" /v "Start" /t REG_DWORD /d "3" /f > nul 2>&3
