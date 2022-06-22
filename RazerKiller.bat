0d4d3849892276b327c8f9b4ba12278d22fb5e8d
DepriFromEarth <107975725+DepriFromEarth@users.noreply.github.com>
2022-06-22 13:20:43 -0400

    Add files via upload

---
+++ RazerKiller.bat
@@ -0,0 +1,6 @@
+taskkill /f /im "Razer Synapse 3.exe"
+taskkill /f /im "Razer Synapse Service.exe"
+taskkill /f /im "Razer Synapse Service Process.exe"
+taskkill /f /im "GameManagerService.exe"
+taskkill /f /im "Razer Central.exe"
+net stop RzActionSvc /y
